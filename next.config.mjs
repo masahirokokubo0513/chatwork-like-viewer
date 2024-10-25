c5eb251b1bfb611ec03e5f3188b0f1b1612fe62e
masahirokokubo0513 <19896624+masahirokokubo0513@users.noreply.github.com>
2024-10-26 03:07:30 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
-const isProd = process.env.NODE_ENV === 'production'
-const prefixPath = !isProd ? '/sub-derectory' : ''
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    assetPrefix: prefixPath,
-    basePath: prefixPath,
-    reactStrictMode: true
-  };
+  output: 'export',
+  // GitHub Pagesのリポジトリ名を設定
+  basePath: '/chatwork-like-viewer',
+  // アセットプレフィックスを設定
+  assetPrefix: '/chatwork-like-viewer/',
+};
 
 export default nextConfig;
